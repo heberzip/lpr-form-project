cc8c715923ada0b1ddf68820dd64d98373454fad
Aggiovato <aggiovato@gmail.com>
2025-02-24 12:55:02 +0100

    feat: Add router folder to create a new component to contain all sections and the main layout

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,94 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Layout from "../layouts/Layout";
+import SectionTransition from "../components/animations/SectionTransition";
+
+import {
+  Welcome,
+  Company,
+  Communication,
+  Contact,
+  Bank,
+  Fleet,
+  Extras,
+  MeetingPoint,
+  NotFound,
+} from "../components/sections";
+
+export const CRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <SectionTransition>
+            <Welcome />
+          </SectionTransition>
+        ),
+      },
+      {
+        path: "/company",
+        element: (
+          <SectionTransition>
+            <Company />
+          </SectionTransition>
+        ),
+      },
+      {
+        path: "/communication",
+        element: (
+          <SectionTransition>
+            <Communication />
+          </SectionTransition>
+        ),
+      },
+      {
+        path: "/contact",
+        element: (
+          <SectionTransition>
+            <Contact />
+          </SectionTransition>
+        ),
+      },
+      {
+        path: "/bank",
+        element: (
+          <SectionTransition>
+            <Bank />
+          </SectionTransition>
+        ),
+      },
+      {
+        path: "/fleet",
+        element: (
+          <SectionTransition>
+            <Fleet />
+          </SectionTransition>
+        ),
+      },
+      {
+        path: "/extras",
+        element: (
+          <SectionTransition>
+            <Extras />
+          </SectionTransition>
+        ),
+      },
+      {
+        path: "/meeting-point",
+        element: (
+          <SectionTransition>
+            <MeetingPoint />
+          </SectionTransition>
+        ),
+      },
+    ],
+    errorElement: (
+      <SectionTransition>
+        <NotFound />
+      </SectionTransition>
+    ),
+  },
+]);
