a25d12697566ce9f98cb23a45e8099b64a12c83b
Aggiovato <aggiovato@gmail.com>
2025-02-28 01:27:50 +0100

    style: Add 404 page new style with a new Zipy image

--- frontend/src/components/sections/NotFound.tsx
+++ frontend/src/components/sections/NotFound.tsx
@@ -13,12 +13,31 @@ const NotFound = () => {
   return (
     <section
       id="not-found"
-      className="flex flex-col items-center justify-center"
+      className="flex flex-col items-center justify-center text-center space-y-4 p-6"
     >
-      <img src={zipy_404} alt="404" className="min-w-sm w-md opacity-90" />
+      <img
+        src={zipy_404}
+        alt="404"
+        className="min-w-2xs w-xs md:w-md opacity-90"
+      />
+      <h2 className="text-2xl font-bold text-zip-blue2-500">Are you lost?</h2>
+      <p className="text-gray-700 max-w-md px-6 text-lg leading-relaxed">
+        It looks like the page you're looking for doesn't exist or has been
+        moved. But don't worry!{" "}
+        <span className="text-zip-yellow-500 font-semibold">
+          We're here to guide you back on track.
+        </span>
+      </p>
+      <p className="text-gray-500 max-w-md px-4 italic">
+        Click the button below to return to the home page and continue your
+        journey with us.
+      </p>
       <Link to={`/?supplier=${supplier}&airports=${airports.join(",")}`}>
-        <button className="bg-zip-blue2-500 text-white px-6 py-1 min-w-[150px] rounded-md shadow-xl hover:bg-zip-blue2-600 active:bg-zip-blue2-800 active:size-0.95 transition disabled:opacity-50">
-          home
+        <button
+          className="mt-4 bg-zip-blue2-500 text-white px-6 py-2 min-w-[150px] rounded-md shadow-xl 
+          hover:bg-zip-blue2-600 active:bg-zip-blue2-800 active:size-0.95 transition duration-200"
+        >
+          Home
         </button>
       </Link>
     </section>
