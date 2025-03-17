3537f8450c3693ee31b552d68d9bdd6f369037d5
Aggiovato <heber@ziptransfers.com>
2025-03-17 18:09:17 +0100

    feat: Add lazy loading to improve fast loading
    Add 'CLoading' component

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,21 +1,25 @@
 // EXTERNAL MODULES
 import { createBrowserRouter } from "react-router-dom";
+import { Suspense, lazy } from "react";
 // LAYOUTS
 import Layout from "@layouts/Layout";
-// SECTIONS
-import {
-  Welcome,
-  Company,
-  Communication,
-  Contact,
-  Bank,
-  Fleet,
-  Extras,
-  MeetingPoint,
-  NotFound,
-} from "@sections/.";
+import CLoading from "@customs/CLoading";
+
 // ANIMATIONS
 import SectionTransition from "@animations/SectionTransition";
+
+// Import lazy
+const Welcome = lazy(() => import("@sections/Welcome"));
+const Company = lazy(
+  () => import(/* webpackPrefetch: true */ "@sections/Company")
+);
+const Communication = lazy(() => import("@sections/Communication"));
+const Contact = lazy(() => import("@sections/Contact"));
+const Bank = lazy(() => import("@sections/Bank"));
+const Fleet = lazy(() => import("@sections/Fleet"));
+const Extras = lazy(() => import("@sections/Extras"));
+const MeetingPoint = lazy(() => import("@sections/MeetingPoint"));
+const NotFound = lazy(() => import("@sections/NotFound"));
 
 /******************************************************************************/
 
@@ -27,72 +31,90 @@ export const CRouter = createBrowserRouter([
       {
         path: "/",
         element: (
-          <SectionTransition>
-            <Welcome />
-          </SectionTransition>
+          <Suspense fallback={<CLoading />}>
+            <SectionTransition>
+              <Welcome />
+            </SectionTransition>
+          </Suspense>
         ),
       },
       {
         path: "/company",
         element: (
-          <SectionTransition>
-            <Company />
-          </SectionTransition>
+          <Suspense fallback={<CLoading />}>
+            <SectionTransition>
+              <Company />
+            </SectionTransition>
+          </Suspense>
         ),
       },
       {
         path: "/communication",
         element: (
-          <SectionTransition>
-            <Communication />
-          </SectionTransition>
+          <Suspense fallback={<CLoading />}>
+            <SectionTransition>
+              <Communication />
+            </SectionTransition>
+          </Suspense>
         ),
       },
       {
         path: "/contact",
         element: (
-          <SectionTransition>
-            <Contact />
-          </SectionTransition>
+          <Suspense fallback={<CLoading />}>
+            <SectionTransition>
+              <Contact />
+            </SectionTransition>
+          </Suspense>
         ),
       },
       {
         path: "/bank",
         element: (
-          <SectionTransition>
-            <Bank />
-          </SectionTransition>
+          <Suspense fallback={<CLoading />}>
+            <SectionTransition>
+              <Bank />
+            </SectionTransition>
+          </Suspense>
         ),
       },
       {
         path: "/fleet",
         element: (
-          <SectionTransition>
-            <Fleet />
-          </SectionTransition>
+          <Suspense fallback={<CLoading />}>
+            <SectionTransition>
+              <Fleet />
+            </SectionTransition>
+          </Suspense>
         ),
       },
       {
         path: "/extras",
         element: (
-          <SectionTransition>
-            <Extras />
-          </SectionTransition>
+          <Suspense fallback={<CLoading />}>
+            <SectionTransition>
+              <Extras />
+            </SectionTransition>
+          </Suspense>
         ),
       },
       {
         path: "/meeting-point",
         element: (
-          <SectionTransition>
-            <MeetingPoint />
-          </SectionTransition>
+          <Suspense fallback={<CLoading />}>
+            <SectionTransition>
+              <MeetingPoint />
+            </SectionTransition>
+          </Suspense>
         ),
       },
     ],
     errorElement: (
-      <SectionTransition>
-        <NotFound />
-      </SectionTransition>
+      <Suspense fallback={<CLoading />}>
+        <SectionTransition>
+          <NotFound />
+        </SectionTransition>
+      </Suspense>
     ),
   },
 ]);
